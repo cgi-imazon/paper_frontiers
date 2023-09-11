07b0041121a093fbe528b0acf785830ad0e3d8d5
Jailson <engsoaresfilho@gmail.com>
2023-09-11 14:42:09 -0300

    update

--- accuracy/02_chart_area_adj.R
+++ accuracy/02_chart_area_adj.R
@@ -2,7 +2,7 @@ library('ggplot2') # nolint: single_quotes_linter.
 library('reshape2') # nolint: single_quotes_linter.
 library('ggthemes')  # nolint: single_quotes_linter.
 library('scales')  # nolint: single_quotes_linter.
-
+library('dplyr') # nolint: single_quotes_linter.
 
 
 
@@ -35,61 +35,70 @@ color_classes <- c(
     "#ff8C00"
 )
 
-
+classes_colors <- as.list(setNames(color_classes, order_classes)) # nolint
 
 
 
 # input data
 data <- read.csv(path_table_area)
-data <- data[, c("variable", "area", "area_adjusted", "year")]
 
-data_melted <- melt(
-    data, na.rm = FALSE,
-    value.name = "value",
-    variable.name = "metric",
-    id = c("variable", "year")
-)
+data_f <- data[, c(
+    "variable",
+    "area",
+    "area_adjusted",
+    "inferior_limit",
+    "superior_limit",
+    "year"
+)]
 
 
 
-
-# rename values
-data_melted$metric <- as.character(data_melted$metric)
-data_melted$metric[data_melted$metric == "area"] <- "Pixel-counting area"
-data_melted$metric[data_melted$metric == "area_adjusted"] <- "Adjusted area"
-data_melted$variable[data_melted$variable == "Rock Outcrop"] <- "Outcrop"
+# rename classes
+data_f$variable[data_f$variable == "Rock Outcrop"] <- "Outcrop"
+data_f$variable <- as.character(data_f$variable)
 
 
 
 
-# order class charts
-data_melted$variable <- factor(
-    data_melted$variable,
+# order dataframe
+data_f$variable <- factor(
+    data_f$variable,
     levels = order_classes
 )
 
 
 
+data_f$diff <- (data_f$superior_limit - data_f$inferior_limit)
 
 
 # display chart
 base <- ggplot(
-    data_melted,
-    aes(year)) +
-    geom_line(aes(y = value, linetype = metric, color = variable), size = 1.5) +
+    data_f,
+    aes(
+        x = year,
+        ymin = inferior_limit,
+        ymax = superior_limit,
+        color = variable,
+        linetype = variable
+    )) +
+    geom_line(
+        aes(y = area, linetype = "Pixel-counting area"), size = 1.5) +
+    geom_line(
+        aes(y = area_adjusted, linetype = "Area adjusted"), size = 1.5) +
+    geom_ribbon(alpha = .2, show.legend = FALSE, aes(fill = variable)) +
     xlab("") + ylab("Area (Mha)") +
     scale_x_continuous(breaks = seq(1985, 2022, by = 6)) +
     scale_y_continuous(
         labels = unit_format(unit = "", scale = 1e-6),
         limits = c(0, NA)) +
     scale_color_manual(values = color_classes, guide = "none") +
+    scale_fill_manual(values = classes_colors) +
+    scale_linetype_manual(
+        values = c("Pixel-counting area" = "solid", "Area adjusted" = "dotted"),
+        labels =  c("Area Adjusted", "Pixel-counting area")) +
+
     facet_wrap(~variable, scales = "free", nrow = 5, ncol = 2) +
     theme_pander() +
     theme(legend.position = "bottom", legend.title = element_blank())
 
-
-#ggsave(
-#    filename = 'paper_c8/accuracy/area_chart_ii.png', plot = base, units = 'in',
-#    width = 9, height = 8, dpi = 3000) # nolint
-
-print(base, width = 1500, height = 700)
+print(base)
